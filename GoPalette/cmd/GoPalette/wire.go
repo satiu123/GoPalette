5fb1a037295a6ecbb5a5792cf2ba94c2056f4f4c
satiu <s13658395102@gmail.com>
2026-04-04 17:10:10 +0800

    feat:初始化kratos结构

---
+++ GoPalette/cmd/GoPalette/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"GoPalette/internal/biz"
+	"GoPalette/internal/conf"
+	"GoPalette/internal/data"
+	"GoPalette/internal/server"
+	"GoPalette/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
